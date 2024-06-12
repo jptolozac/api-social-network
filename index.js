e5086e441365d2e55a49f37da00b2ebd5065db38
inesmariao <inesmao.misena@gmail.com>
2024-06-11 20:51:36 -0500

    initial project and start-project.bat

---
+++ index.js
@@ -0,0 +1,16 @@
+// Importaciones
+
+
+// Conexión a la BD
+
+
+// Configurar cors: permite que las peticiones se hagan correctamente
+
+
+// Conversión de datos (body a objetos JS)
+
+
+// Configurar rutas
+
+
+// Configurar el servidor para escuchar las peticiones HTTP
