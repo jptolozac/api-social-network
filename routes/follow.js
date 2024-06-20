7365f864dd3076ca1fcbdb04df0ab248b7eecdc1
inesmariao <inesmao.misena@gmail.com>
2024-06-19 21:57:44 -0500

    method saveFollow OK

--- routes/follow.js
+++ routes/follow.js
@@ -1,10 +1,12 @@
 // Importaciones
 import { Router } from "express";
 const router = Router();
-import { testFollow } from "../controllers/follow.js";
+import { testFollow, saveFollow } from "../controllers/follow.js";
+import { ensureAuth } from "../middlewares/auth.js";
 
 // Definir las rutas
 router.get('/test-follow', testFollow);
+router.post("/follow", ensureAuth, saveFollow);
 
 // Exportar el Router
 export default router;
