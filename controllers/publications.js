05efbb833c07ee737e847ffcbcbab576755f5d0d
inesmariao <inesmao.misena@gmail.com>
2024-06-21 20:53:33 -0500

    controller publications OK

--- controllers/publications.js
+++ controllers/publications.js
@@ -1,6 +1,91 @@
+import Publication from "../models/publication.js"
+
 // Acciones de prueba
 export const testPublication = (req, res) => {
   return res.status(200).send({
     message: "Mensaje enviado desde el controlador: publications.js"
   });
+}
+
+// Método para hacer una publicación
+export const savePublication = async (req, res) => {
+  try {
+    
+    // Obtener datos del body
+    const params = req.body;
+
+    // Verificar que llegue desde el body el parámetro text con su información
+    if (!params.text){
+      return res.status(400).send({
+        status: "error",
+        message: "Debes enviar el texto de la publicación"
+      });
+    }
+
+    // Crear el objeto del modelo
+    let newPublication = new Publication(params);
+
+    // Agregar la información del usuario autenticado al objeto de la nueva publicación
+    newPublication.user_id = req.user.userId;
+
+    // Guardar la nueva publicación en la BD
+    const publicationStored = await newPublication.save();
+
+    // Verificar si se guardó la publicación en la BD (si existe publicationStored)
+    if (!publicationStored){
+      return res.status(500).send({
+        status: "error",
+        message: "No se ha guardado la publicación"
+      });
+    }
+
+    // Devolver respuesta exitosa 
+    return res.status(200).send({
+      status: "success",
+      message: "¡Publicación creada con éxito!",
+      publicationStored
+    });
+
+  } catch (error) {
+    console.log("Error al crear la publicación:", error);
+    return res.status(500).send({
+      status: "error",
+      message: "Error al crear la publicación"
+    });
+  }
+}
+
+// Método para mostrar la publicación
+export const showPublication = async (req, res) => {
+  try {
+
+    // Obtener el id de la publicación de la url
+    const publicationId = req.params.id;
+
+    // Buscar la publicación por id desde la BD
+    const publicationStored = await Publication.findById(publicationId)
+    .populate('user_id', 'name last_name');
+
+    // Verificar si se encontró la publicación
+    if (!publicationStored) {
+      return res.status(500).send({
+        status: "error",
+        message: "No existe la publicación"
+      });
+    }
+
+    // Devolver respuesta exitosa 
+    return res.status(200).send({
+      status: "success",
+      message: "Publicación encontrada",
+      publication: publicationStored
+    });
+
+  } catch (error) {
+    console.log("Error al mostrar la publicación:", error);
+    return res.status(500).send({
+      status: "error",
+      message: "Error al mostrar la publicación"
+    });
+  }
 }
--- routes/publications.js
+++ routes/publications.js
@@ -1,10 +1,13 @@
 // Importaciones
 import { Router } from "express";
 const router = Router();
-import { testPublication } from "../controllers/publications.js";
+import { testPublication, savePublication, showPublication } from "../controllers/publications.js";
+import { ensureAuth } from "../middlewares/auth.js";
 
 // Definir las rutas
 router.get('/test-publication', testPublication);
+router.post('/publication', ensureAuth, savePublication);
+router.get('/show-publication/:id', ensureAuth, showPublication);
 
 // Exportar el Router
 export default router;
